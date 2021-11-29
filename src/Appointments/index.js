12d4817efc7d789a2c228fc7a0793195c1f753b7
Zach Smith <Lemelisk27@gmail.com>
2021-11-29 00:48:25 -0800

    added patient information to the appointment tooltip

--- src/Appointments/index.js
+++ src/Appointments/index.js
@@ -6,9 +6,6 @@ function Appointments (props) {
     return (
         <div className="zs-appointments d-flex flex-row pt-3">
             <div className="zs-appt-card d-flex flex-column col-11 m-auto rounded">
-                <div className="d-flex flex-row col-11 mx-auto mt-4 justify-content-between border-bottom border-dark pb-4">
-                    <h1>Appointments</h1>
-                </div>
                 <div className="mt-3 mx-auto col-11">
                     <Calendar />
                 </div>
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import Auth from "../utils/auth"
 import API from "../utils/API"
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
 import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
@@ -24,10 +26,10 @@ function Calendar (props) {
     const [addedAppointment, changeAddedAppointment] = useState({})
     const [appointmentChanges, changeAppointmentChanges] = useState({})
     const [editingAppointment, changeEditingAppointment] = useState(undefined)
+    const [updatePage, setUpdatePage] = useState(true)
     const token = Auth.getToken()
 
     const commitChanges = ({ added, changed, deleted }) => {
-        const newToken = Auth.getToken()
         if (added) {
             console.log("added")
             console.log(added)
@@ -37,9 +39,10 @@ function Calendar (props) {
             console.log(changed)
         }
         if (deleted !== undefined) {
-            API.deleteAppt(deleted,newToken)
+            API.deleteAppt(deleted,token)
             .then(res=>{
                 console.log(res)
+                setUpdatePage(!updatePage)
             })
             .catch(err=>{
                 console.log(err)
@@ -52,13 +55,50 @@ function Calendar (props) {
         .then(res=>{
             setAppointments(res.data)
         })
+        .catch(err=>{
+            console.log(err)
+        })
     },[])
+
+    useEffect (() => {
+        API.getAppts(token)
+        .then(res=>{
+            setAppointments(res.data)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    },[updatePage])
+
+    const style = ({ palette }) => ({
+        textCenter: {
+            textAlign: 'center'
+        }
+    })
+
+    const Content = withStyles(style, { name: 'Content' })(({ children, appointmentData, classes, ...restProps}) => (
+        <AppointmentTooltip.Content {...restProps} appointmentData={appointmentData}>
+            <Grid container alignItems="center">
+                <Grid className="mx-auto col-11">
+                    <h5 className="my-1 border-bottom border-secondary">{appointmentData.Animal.name}</h5>
+                    <div className="col-11 mx-auto">
+                        <p className="mb-0">Species: {appointmentData.Animal.Species.species}</p>
+                        <p className="mb-0">Breed: {appointmentData.Animal.Breed.breed}</p>
+                        <div className="mt-2 p-2 col-12 border border-secondary">
+                            <p className="mb-0">Notes:</p>
+                            <p className="mb-0">{appointmentData.notes}</p>
+                        </div>
+                    </div>
+                </Grid>
+            </Grid>
+        </AppointmentTooltip.Content>
+    ))
 
     return (
         <Paper>
             <Scheduler
                 data={appointments}
-                height={590}
+                height={700}
             >
                 <ViewState
                     defaultCurrentViewName="Week"
@@ -92,6 +132,7 @@ function Calendar (props) {
                     showCloseButton
                     showOpenButton
                     showDeleteButton
+                    contentComponent={Content}
                 />
                 <AppointmentForm />
             </Scheduler>

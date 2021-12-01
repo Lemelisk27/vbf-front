d51b25a633377e08d2f23d94e60a95378be7fe7a
Zach Smith <Lemelisk27@gmail.com>
2021-12-01 09:38:31 -0800

    users can add clients to the sales page

--- src/pages/Sales/index.js
+++ src/pages/Sales/index.js
@@ -1,23 +1,62 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./style.css"
 import 'react-bootstrap-typeahead/css/Typeahead.css'
 import { Typeahead } from 'react-bootstrap-typeahead'
+import Auth from "../../utils/auth"
+import API from "../../utils/API"
 
 function Sales (props) {
-    const [productList, setProductList] = useState(false)
+    const token = Auth.getToken()
+    const [productList] = useState(false)
+    const [selectedClient, setSelectedClient] = useState([])
+    const [clientList, setClientList] = useState([])
+    const [clientSelected, setClientSelected] = useState(false)
+
+    useEffect (()=>{
+        API.getClients(token)
+        .then(res=>{
+            const tempArray = []
+            for (let i = 0; i < res.data.length; i++) {
+                const tempObj = {
+                    label: res.data[i].full_name,
+                    id: res.data[i].id
+                }
+                tempArray.push(tempObj)
+            }
+            setClientList(tempArray)
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    // eslint-disable-next-line
+    },[])
+
+    const addClient = (e) => {
+        e.preventDefault()
+        setClientSelected(true)
+    }
 
     return (
         <div className="zs-sales d-flex flex-row pt-3">
             <div className="zs-sales-card d-flex flex-column col-11 m-auto rounded">
                 <div className="d-flex flex-row col-11 mx-auto mt-4 justify-content-between border-bottom border-dark pb-4">
                     <h1>Sales</h1>
-                    <div className="d-flex flex-row col-8 justify-content-end">
-                        <button className="bg-primary text-light col-3 mx-3 rounded align-self-end" style={{height: "33px"}}>Add</button>
-                        <div className="d-flex flex-column col-5">
-                            <label className="text-end">Client</label>
-                            <input type="text" name="client" placeholder="Type Here to Search..."></input>
+                    {clientSelected ? (
+                        <h1>{selectedClient[0].label}</h1>
+                    ):(
+                        <div className="d-flex flex-row col-8 justify-content-end">
+                            <button className="bg-primary text-light col-3 mx-3 rounded align-self-end" onClick={addClient}>Add</button>
+                            <div className="d-flex flex-column col-5 align-self-end">
+                                <Typeahead
+                                    id="client-list"
+                                    onChange={setSelectedClient}
+                                    options={clientList}
+                                    placeholder="Select a Client..."
+                                    selected={selectedClient}
+                                />
+                            </div>
                         </div>
-                    </div>
+                    )}
                 </div>
                 <div className="d-flex flex-column col-11 mx-auto mt-5">
                     <h3>Products</h3>
@@ -31,7 +70,7 @@ function Sales (props) {
                         <p className="text-center my-auto">No Products Entered</p>
                     )}
                 </div>
-                <div className="d-flex flex-column col-11 mx-auto">
+                <div className="d-flex flex-row col-11 mx-auto justify-content-between">
                     <div className="d-flex flex-column border border-dark col-2 mt-2 justify-content-between">
                         <table className="mx-2">
                             <tbody>
@@ -57,6 +96,10 @@ function Sales (props) {
                                 </tr>
                             </tbody>
                         </table>
+                    </div>
+                    <div className="d-flex flex-column col-3 align-self-end">
+                        <button className="bg-primary text-light rounded mb-2">Save</button>
+                        <button className="bg-primary text-light rounded" onClick={() => window.location.reload()}>Cancel</button>
                     </div>
                 </div>
             </div>
